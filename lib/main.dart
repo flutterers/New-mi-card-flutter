0371be8d873617486e5f9891a8b3243154a892dc
MennaCC <menna.elsisy@gmail.com>
2023-02-19 23:07:19 +0200

    Closes #5 Reformat Contact Cards using flutter Cards and ListTiles, Section 6 | vid 47

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'dart:convert';
 
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
+// import 'package:flutter/services.dart';
 
 void main() {
   runApp(const MyApp());
@@ -21,9 +21,9 @@ class MyApp extends StatelessWidget {
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
               Container(
-                margin: const EdgeInsets.fromLTRB(50.0, 0.0, 50.0, 10.0),
+                margin: const EdgeInsets.symmetric(horizontal: 50),
                 child: const CircleAvatar(
-                  radius: 80.0,
+                  radius: 120.0,
                   backgroundColor: Colors.yellow,
                   foregroundImage: AssetImage("images/cute-dino.jpg"),
                 ),
@@ -38,7 +38,7 @@ class MyApp extends StatelessWidget {
                 ),
               ),
               Text(
-                "Cute Dino Passing By",
+                "CuteDino PassingBy",
                 style: TextStyle(
                   fontWeight: FontWeight.bold,
                   fontSize: 20.0,
@@ -47,58 +47,49 @@ class MyApp extends StatelessWidget {
                   color: Colors.teal.shade100,
                 ),
               ),
-              Container(
-                color: Colors.white,
-                margin: const EdgeInsets.fromLTRB(25, 25, 25, 8),
-                padding: const EdgeInsets.symmetric(
-                    vertical: 10.0, horizontal: 40.0),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Icon(
-                      Icons.phone,
-                      color: Colors.teal,
-                      // size: 25,
-                    ),
-                    SizedBox(width: 10.0),
-                    Text(
-                      "+044 123 456 789",
-                      style: TextStyle(
-                        color: Colors.teal.shade900,
-                        fontSize: 20.0,
-                        fontFamily: 'SourceSansPro',
-                      ),
-                    )
-                  ],
+              SizedBox(
+                width: 125.0,
+                height: 35.0,
+                child: Divider(
+                  color: Colors.teal.shade100,
+                  thickness: 1.0,
                 ),
               ),
-              Container(
+              Card(
                 color: Colors.white,
-                margin: const EdgeInsets.symmetric(horizontal: 25.0),
-                padding: const EdgeInsets.symmetric(
-                    vertical: 10.0, horizontal: 40.0),
-                // decoration: BoxDecoration(
-                //     borderRadius: BorderRadius.circular(5.0),
-                //     color: Colors.white),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Icon(
+                margin: const EdgeInsets.fromLTRB(20, 0, 20, 0),
+                child: ListTile(
+                  leading: const Icon(
+                    Icons.phone,
+                    color: Colors.teal,
+                  ),
+                  title: Text(
+                    "+044 123 456 789",
+                    style: TextStyle(
+                      color: Colors.teal.shade800,
+                      fontSize: 20.0,
+                      fontFamily: 'SourceSansPro',
+                    ),
+                  ),
+                ),
+              ),
+              Card(
+                color: Colors.white,
+                margin: const EdgeInsets.symmetric(
+                    horizontal: 20.0, vertical: 15.0),
+                child: ListTile(
+                    leading: const Icon(
                       Icons.mail,
                       color: Colors.teal,
-                      // size: 25,
                     ),
-                    SizedBox(width: 10.0),
-                    Text(
+                    title: Text(
                       "cute.dino@gmail.com",
                       style: TextStyle(
-                        color: Colors.teal.shade900,
+                        color: Colors.teal.shade800,
                         fontSize: 20.0,
                         fontFamily: 'SourceSansPro',
                       ),
-                    )
-                  ],
-                ),
+                    )),
               )
             ],
           ),
